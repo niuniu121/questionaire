b3e0b4ef730103d17a3b1a2a43fa827ad0aada93
joy.niuniu121 <joy.18217355998@163.com>
2026-04-28 17:27:05 +1000

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import viteLogo from "../assets/vite.svg";
+import heroImg from "../assets/hero.png";
+import vueLogo from "../assets/vue.svg";
+
+const count = ref(0);
+</script>
+
+<template>
+  <section id="center">
+    <div class="hero">111</div>
+  </section>
+
+  <div class="ticks"></div>
+  <section id="spacer"></section>
+</template>
